6526f744142d556afeebb734586bf7726fcb7198
Gabriel Chamorro <gabriel.n.chamorro@gmail.com>
2024-11-25 19:52:42 -0300

    Convert FrontEnd-PcCarton from submodule to regular directory

---
+++ FrontEnd-PcCarton/src/app/pagina-inicio/pagina-inicio.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiProductosService } from './../servicio/api-productos.service'
+import { RespuestaApiProducto } from './../interfaces/RespuestaApiProducto'
+import { ViewWillEnter, ViewWillLeave } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-pagina-inicio',
+  templateUrl: './pagina-inicio.page.html',
+  styleUrls: ['./pagina-inicio.page.scss'],
+})
+export class PaginaInicioPage implements ViewWillEnter, ViewWillLeave, OnInit {
+  private suscripcionProducto!: Subscription;
+  public datos: RespuestaApiProducto = { OK: false, count: 0, msg: '', registro: [] };
+  constructor(
+    public producto: ApiProductosService,
+  ) { }
+
+
+
+
+  
+
+  ionViewWillEnter(): void {
+    this.suscripcionProducto = this.producto.productos.subscribe(losDatos => {
+      if(losDatos){
+        this.datos = losDatos;
+      }
+    }); 
+    this.producto.obtenerProductos()
+    
+  }
+
+
+  ionViewWillLeave(): void {
+    this.suscripcionProducto.unsubscribe();
+  }
+
+
+  ngOnInit() {
+  }
+
+
+}
---
+++ FrontEnd-PcCarton/src/app/servicio/api-login.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CuerpoLogin } from './../interfaces/CuerpoLogin';
+import { UsuarioLogeado } from './../interfaces/UsuarioLogeado';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login';
+  public usuarioLogeado: UsuarioLogeado | null = null;
+  public accessToken: string | null = null;
+  // Observador de cargando
+  private $cargando = new BehaviorSubject<boolean>(false);
+  public cargando = this.$cargando.asObservable();
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {
+
+  }
+
+  public iniciarSesion(nombre_usuario: string, contrasenia: string){
+    this.$cargando.next(true);
+
+    const cuerpo: CuerpoLogin = {
+      username: nombre_usuario,
+      password: contrasenia
+    }
+    this.http.post<UsuarioLogeado>(this.URL_LOGIN, JSON.stringify(cuerpo), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .subscribe(resultado  => {
+      this.usuarioLogeado = resultado;
+      this.accessToken = resultado.accessToken;
+      this.$cargando.next(false);
+      console.log(resultado);
+      this.router.navigate(['/','productos']);
+    });
+
+  }
+
+  public cerrarSesion(){
+    if(this.usuarioLogeado){
+      this.usuarioLogeado = null;
+      this.accessToken = null;
+    }
+  }
+}
